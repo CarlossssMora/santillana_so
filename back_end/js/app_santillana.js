7519353c4d39dd5b63debc830d1b480dcf040137
MikeFlores19 <mf893397@gmail.com>
2024-11-06 20:59:18 -0600

    ACTUALIZACION FINAL DE BACKEND

--- back_end/js/app_santillana.js
+++ back_end/js/app_santillana.js
@@ -1,14 +1,16 @@
 //IMPORTACION DE HERRAMIENTAS
 import dotenv from 'dotenv';                            
-import express from 'express';
 import connectMongoDB from './config/connectMongoDB.js';
-import cookieParser from 'cookie-parser'; //mantiente la sesion iniciada hasta que decida cerrar
+
+//IMPORTACION DE MIDDLEWARES
+import express from 'express';
+import cookieParser from 'cookie-parser'; 
+import cors from 'cors';
 
 //IMPORTACION DE RUTAS
 import cliente from './routes/cliente.js';  
 import proyecto from './routes/proyecto.js';
 import contacto from './routes/contacto.js';
-//IMPORTACION DE MIDDLEWARES
 
 
 dotenv.config();//Utilizar dotenv para llamar las constantes del archivo .env
@@ -18,8 +20,10 @@ const app=express();
 const PORT= process.env.PORT;
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(cookieParser());
+app.use(express.urlencoded({extended:true})); //Permite que tu aplicación pueda entender y procesar datos que vienen desde formularios HTML. Es útil cuando tienes datos enviados desde formularios, y convierte la información codificada (como name=John&age=30) en un objeto JavaScript.
+app.use(cookieParser());//Permite que tu aplicación lea cookies en las solicitudes entrantes. Las cookies son pequeños fragmentos de datos que los navegadores almacenan y envían al servidor en cada solicitud. Esto es útil para mantener información sobre la sesión del usuario o para recordar preferencias.
+app.use(cors());// Permite que tu servidor acepte solicitudes desde otros dominios. Sin cors, los navegadores bloquearán estas solicitudes, especialmente cuando el frontend y backend están en diferentes servidores.
+
 
 app.use('/',cliente);
 app.use('/',proyecto);
