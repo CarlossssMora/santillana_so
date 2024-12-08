060691e43c3a7e5b0dc202db170f41dc561d87e9
MikeFlores19 <mf893397@gmail.com>
2024-12-08 17:49:51 -0600

    cambios

--- back_end/js/models/proyecto.js
+++ back_end/js/models/proyecto.js
@@ -1,24 +1,3 @@
-<<<<<<< Updated upstream
-import mongoose from 'mongoose';  
-const { Schema } = mongoose;
-
-const proyectoSchema = new Schema({
-    ClienteID: { type: Schema.Types.ObjectId, ref: 'Cliente', required: true },
-    nombre: { type: String, required: true },
-    fechainicio: { type: Date, required: true },
-    fechafin: { type: Date },
-    responsable: { type: String },
-    comentarios: { type: String },
-    avance: { type: Number, min: 0, max: 100, default: 0 },
-    factura: { type: String },
-    monto: { type: Number, required: true },
-    anticipo: { type: Number, default: 0 },
-    restante: { type: Number },
-    statusFactura: { type: String, enum: ['Pendiente', 'Pagado', 'Cancelado'], default: 'Pendiente' },
-});
-
-export default mongoose.model('Proyecto', proyectoSchema);
-=======
 import mongoose from 'mongoose';  
 const { Schema } = mongoose;
 
@@ -39,5 +18,3 @@ const proyectoSchema = new Schema({
 
 export default mongoose.model('Proyecto', proyectoSchema);
 
-
->>>>>>> Stashed changes
