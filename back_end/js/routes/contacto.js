ce6c8bf6085fbb3946d219d6cd9a18abc2a2eeb7
Samuelsoc01 <sorianosamuel430@gmail.com>
2025-01-06 08:42:45 -0600

    Back correccion

--- back_end/js/routes/contacto.js
+++ back_end/js/routes/contacto.js
@@ -15,7 +15,7 @@ router.post('/contacto/nuevo',async(req,res)=>{
     }
 });
 
-//Ruta para obtener todos los proyectos
+//Ruta para obtener todos los contactos
 router.get('/contacto/lista', async (req, res) => {
     try {
         const contactos= await Contacto.find();
@@ -26,16 +26,39 @@ router.get('/contacto/lista', async (req, res) => {
     }
 });
 
-// Ruta para eliminar un cliente
-router.delete('/contacto/eliminar/:id', async (req, res) => {
+router.delete('/contactos/eliminar/:id', async (req, res) => {
     try {
-        const contactoEliminado = await Contacto.findByIdAndDelete(req.params.id);
-        if (!contactoEliminado) return res.status(404).json({ message: "Contacto no encontrado" });
-        res.json({ message: "Contacto eliminado" });
-    } 
-    catch (error) {
-        res.status(500).json({ message: error.message });
+      const citaEliminada = await Contacto.findByIdAndDelete(req.params.id);
+      if (!citaEliminada) {
+        return res.status(404).json({ message: 'Cita no encontrada.' });
+      }
+      res.status(200).json({ message: 'Cita eliminada exitosamente.' });
+    } catch (error) {
+      console.error('Error al eliminar cita:', error.message);
+      res.status(500).json({ message: 'Error al eliminar cita.' });
     }
-});
+  });
+  
+
+
+// Ruta para buscar citas asociadas a un representante por nombre
+router.get('/contactos/citas/:nombre', async (req, res) => {
+    const { nombre } = req.params;
+  
+    try {
+      const citas = await Contacto.find({
+        nombre_representante: new RegExp(nombre, 'i'), // Búsqueda insensible a mayúsculas/minúsculas
+      });
+  
+      if (citas.length === 0) {
+        return res.status(404).json({ message: 'No se encontraron citas.' });
+      }
+  
+      res.status(200).json(citas);
+    } catch (error) {
+      console.error('Error al buscar citas:', error.message);
+      res.status(500).json({ message: 'Error al buscar citas.' });
+    }
+  });
 
 export default router;
